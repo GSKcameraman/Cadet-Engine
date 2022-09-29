60bc9d53d816591c6272a2ec28b28a321e19e82f
GSKcameraman <gskcameraman@gmail.com>
2022-09-29 15:51:56 -0400

    hw2 is done
    bad time figuring out that lz4 is not the one making error, is we should not use strcpy_s() to copy compressed files.

--- src/fs.c
+++ src/fs.c
@@ -42,6 +42,7 @@ typedef struct fs_work_t
 	fs_compress_op_t use_compression;
 	void* buffer;
 	size_t size;
+	size_t compression_size;
 	event_t* done;
 	event_t* com_done;
 	int result;
@@ -92,6 +93,7 @@ fs_work_t* fs_read(fs_t* fs, const char* path, heap_t* heap, bool null_terminate
 		work->use_compression = k_fs_work_op_decompress;
 	}
 	else {
+		//set action to opposite to avoid any misuses
 		work->use_compression = k_fs_work_op_compress;
 	}
 
@@ -111,17 +113,21 @@ fs_work_t* fs_write(fs_t* fs, const char* path, const void* buffer, size_t size,
 	work->done = event_create();
 	work->result = 0;
 	work->null_terminate = false;
-	work->use_compression = k_fs_work_op_compress;
+	
+	work->com_done = event_create();
 	work->fs = fs;
 	if (use_compression)
 	{
 		// HOMEWORK 2: Queue file write work on compression queue!
+		work->use_compression = k_fs_work_op_compress;
 		queue_push(fs->compress_queue, work);
 		event_wait(work->com_done);
 		queue_push(fs->file_queue, work);
 	}
 	else
 	{
+		//set action to opposite to avoid any misuses
+		work->use_compression = k_fs_work_op_decompress;
 		event_signal(work->com_done);
 		queue_push(fs->file_queue, work);
 	}
@@ -222,6 +228,7 @@ static void file_read(fs_work_t* work)
 	}
 	else
 	{
+		event_signal(work->com_done);
 		if (work->null_terminate)
 		{
 			((char*)work->buffer)[work->size] = 0;
@@ -247,16 +254,32 @@ static void file_write(fs_work_t* work)
 		return;
 	}
 
-	DWORD bytes_written = 0;
-	if (!WriteFile(handle, work->buffer, (DWORD)work->size, &bytes_written, NULL))
-	{
-		work->result = GetLastError();
-		CloseHandle(handle);
-		return;
+
+	if (work->use_compression == k_fs_work_op_compress) {
+		DWORD bytes_written = 0;
+		if (!WriteFile(handle, work->buffer, (DWORD)work->compression_size, &bytes_written, NULL))
+		{
+			work->result = GetLastError();
+			CloseHandle(handle);
+			return;
+		}
+
+		work->compression_size = bytes_written;
+
+		heap_free(work->heap, work->buffer);
 	}
+	else {
+		DWORD bytes_written = 0;
+		if (!WriteFile(handle, work->buffer, (DWORD)work->size, &bytes_written, NULL))
+		{
+			work->result = GetLastError();
+			CloseHandle(handle);
+			return;
+		}
 
-	work->size = bytes_written;
+		work->size = bytes_written;
 
+	}
 	CloseHandle(handle);
 
 	event_signal(work->done);
@@ -289,10 +312,13 @@ static int file_thread_func(void* user)
 
 
 static void file_compress(fs_work_t* work) {
-	void* buffer_temp = heap_alloc(work->heap, work->size, 8);
-	int compressed_size = LZ4_compress_default((char*)work->buffer, (char*)buffer_temp, (int)work->size, (int)work->size);
+	void* buffer_temp = heap_alloc(work->heap, work->size + sizeof(int), 8);
+	int compressed_size = LZ4_compress_default((char*)work->buffer, (char*)buffer_temp + sizeof(int), (int)work->size, (int)work->size);
 	
 	if (compressed_size != 0) {
+		
+		*(int*)buffer_temp = (int) work->size;
+		work->compression_size = compressed_size + sizeof(int);
 		work->buffer = buffer_temp;
 	}
 	else {
@@ -303,8 +329,11 @@ static void file_compress(fs_work_t* work) {
 }
 
 static void file_decompress(fs_work_t* work) {
-	char* buffer_temp = heap_alloc(work->heap, work->null_terminate ? (int)(work->size * 255 + 1) : (int)work->size * 255, 8);
-	int decompress_size = LZ4_decompress_safe(work->buffer, buffer_temp, (int)work->size, (int)work->size * 2);
+	int decompressed_size = *(int*)(work->buffer);
+	char* buffer_temp = heap_alloc(work->heap, work->null_terminate ? (decompressed_size + 1) : decompressed_size, 8);
+	char* compressed = (char*)work->buffer + sizeof(int);
+	int compressed_size = (int)(work->size - sizeof(int));
+	int decompress_size = LZ4_decompress_safe(compressed, buffer_temp, compressed_size, decompressed_size);
 	if (decompress_size > 0) {
 		work->size = decompress_size;
 		heap_free(work->heap, work->buffer);
